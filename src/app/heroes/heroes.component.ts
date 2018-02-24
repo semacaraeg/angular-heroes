a8b5ef85579f0162acb4b74157975e4f7d4ffa0c
Sarah <semacaraeg@gmail.com>
2018-02-24 06:55:52 +0000

    Finished step 7: Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,15 +17,12 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
     this.getHeroes();
   }
-  
-  
-  onSelect(hero : Hero) {
-    this.selectedHero = hero;
-  }
+
   
    getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
+  
 
 }
